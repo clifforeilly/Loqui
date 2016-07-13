165dd33bd60109ae9bc4d62825f78ab8894fdd37
O'Reilly <CO17@anglia.ac.uk>
2016-07-13 10:46:48 +0100

    Initial commit

---
+++ src/Loqui.java
@@ -0,0 +1,15 @@
+/**
+ * Created by co17 on 13/07/2016.
+ */
+public class Loqui {
+
+
+    public static void main (String[] args)
+    {
+        System.out.println("started Loqui ...");
+
+
+
+        System.out.println("ended Loqui ...");
+    }
+}
